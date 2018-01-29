5f2982506952f8a414e7129a6ff8396f9b9f1a9f
glindy89 <35814731+glindy89@users.noreply.github.com>
2018-01-29 15:52:35 -0800

    Update AppDelegate.h

--- AppDelegate.h
+++ AppDelegate.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
+@property (copy, readwrite) NSString * customerId;
 @property (strong, nonatomic) UIWindow *window;
 
 
